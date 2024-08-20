bbdd079875112fcdeebd179773ac0f1652c6828d
Geoffrey Challen <challen@illinois.edu>
2024-08-19 21:36:15 -0500

    JS updates.

--- js/types/src/universal.ts
+++ js/types/src/universal.ts
@@ -3,6 +3,7 @@ import { Languages } from "./languages"
 import { SubmissionType } from "./submission"
 
 export const UniversalSubmission = Record({
+  type: SubmissionType,
   id: String,
   timestamp: InstanceOf(Date),
   question: Record({
@@ -12,15 +13,15 @@ export const UniversalSubmission = Record({
     Partial({
       author: String,
       version: String,
+      contentHash: String,
     }),
   ),
-  author: String,
-  type: SubmissionType,
   contents: String,
   results: Unknown,
 }).And(
   Partial({
     email: String,
+    browserID: String,
   }),
 )
 export type UniversalSubmission = Static<typeof UniversalSubmission>
