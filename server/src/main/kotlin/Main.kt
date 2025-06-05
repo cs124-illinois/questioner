6fc2d0b5d037a2da83796b2b1fb378deb7f6c1ab
Geoffrey Challen <challen@illinois.edu>
2025-06-05 14:33:17 -0500

    Reduce logging verbosity.

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -244,7 +244,7 @@ fun main(@Suppress("unused") unused: Array<String>): Unit = runBlocking {
                 ).dumpHeap(heapDumpName, false)
             }
         }
-        logger.debug("Questioner heartbeat $index completed in ${(warmTime / 1000.0).round(1)}s. ${printMemory()}. Cache ${questionCache.estimatedSize()} / $questionCacheSize.")
+        logger.trace("Questioner heartbeat $index completed in ${(warmTime / 1000.0).round(1)}s. ${printMemory()}. Cache ${questionCache.estimatedSize()} / $questionCacheSize.")
     }.launchIn(GlobalScope)
 
     embeddedServer(Netty, port = 8888, module = Application::questioner).start(wait = true)
